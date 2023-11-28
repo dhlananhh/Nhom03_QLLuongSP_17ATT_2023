0665570cbab9c18463fad70ee6fbb060d67eaf3d
Ly Quoc Minh <minh@gmail.com>
2023-11-28 22:38:28 +0700

    class icon

---
+++ src/gui/Icon.java
@@ -0,0 +1,9 @@
+package gui;
+
+import javax.swing.ImageIcon;
+
+public class Icon {
+	public ImageIcon iconThem = new ImageIcon("icons/iconpack/black/stroke/add.png");
+	public ImageIcon iconTim = new ImageIcon("icons/iconpack/black/stroke/glass.png");
+	public ImageIcon iconXoa = new ImageIcon("icons/iconpack/black/stroke/trash.png");
+}
